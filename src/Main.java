bd699102042ce70617fbbc47fbaef350c4c1b801
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-09-28 12:53:21 +0300

    add repository1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int check = 100;
+        int refill = 1299;
+        int finalScor = 0;
+        int kolvobonus = 0;
+        if (refill >= 1000) {
+            finalScor = refill / 100 + check + refill;
+            kolvobonus = refill / 100;
+        } else {
+            finalScor = refill + check;
+            kolvobonus = 0;
+        }
+
+        System.out.println("Итоговый счёт = " + finalScor);
+        System.out.println("Количество бонусных рублей = " + kolvobonus);
     }
 }
